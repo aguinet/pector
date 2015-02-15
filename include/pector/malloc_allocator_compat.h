2f3b7e4c88b3634d077f158d8a90553981b8c0a7
Adrien Guinet <aguinet@quarkslab.com>
2015-02-15 13:19:32 +0100

    Change malloc_usable_size for Linux with the __linux__ macro
    Suggested by Carter Li.
    Also add check for GNU/Hurd.

--- include/pector/malloc_allocator_compat.h
+++ include/pector/malloc_allocator_compat.h
@@ -7,7 +7,7 @@
 #include <malloc.h>
 #endif
 
-#if defined __GNUC__ || defined _WIN32 || defined __APPLE__ || defined __FreeBSD__
+#if defined __linux__ || defined __gnu_hurd__ || defined _WIN32 || defined __APPLE__ || defined __FreeBSD__
 
 #define PT_SIZE_AWARE_COMPAT
 
@@ -15,7 +15,7 @@
 #define PT_MALLOC_USABLE_SIZE(p) _msize(p)
 #elif defined __APPLE__
 #define PT_MALLOC_USABLE_SIZE(p) malloc_size(p)
-#elif defined __GNUC__ || defined __FreeBSD__
+#elif defined __gnu_hurd__ || __linux__ || defined __FreeBSD__
 #define PT_MALLOC_USABLE_SIZE(p) malloc_usable_size(p)
 #endif
 
