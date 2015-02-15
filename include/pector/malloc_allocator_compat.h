b483dd161b054adcff933fd073550bf8f7cc1c47
Adrien Guinet <aguinet@quarkslab.com>
2015-02-15 13:01:06 +0100

    Add FreeBSD as platform supporting malloc_usable_size

--- include/pector/malloc_allocator_compat.h
+++ include/pector/malloc_allocator_compat.h
@@ -7,16 +7,16 @@
 #include <malloc.h>
 #endif
 
-#if defined __GNUC__ || defined _WIN32 || defined __APPLE__
+#if defined __GNUC__ || defined _WIN32 || defined __APPLE__ || defined __FreeBSD__
 
 #define PT_SIZE_AWARE_COMPAT
 
-#if defined __GNUC__ && !defined _WIN32 && !defined __APPLE__
-#define PT_MALLOC_USABLE_SIZE(p) malloc_usable_size(p)
-#elif defined _WIN32
+#if defined _WIN32
 #define PT_MALLOC_USABLE_SIZE(p) _msize(p)
 #elif defined __APPLE__
-#define PTçMALLOC_USABLE_SIZE(p) malloc_size(p)
+#define PT_MALLOC_USABLE_SIZE(p) malloc_size(p)
+#elif defined __GNUC__ || defined __FreeBSD__
+#define PT_MALLOC_USABLE_SIZE(p) malloc_usable_size(p)
 #endif
 
 #endif // defined __GNUC__ || defined _WIN32 || defined __APPLE__
